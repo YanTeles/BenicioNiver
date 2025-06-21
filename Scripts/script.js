7d969df012dd192aed3f05bb0469d6d23618e5d9
Yan <Yanpereirateles@gmail.com>
2025-06-21 15:18:14 -0300

    make the other area

--- Scripts/script.js
+++ Scripts/script.js
@@ -31,4 +31,49 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+
+    const familySwiper = new Swiper('.family-gallery-swiper', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        loop: true,
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        pagination: {
+            el: '.family-gallery-swiper .swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.family-gallery-swiper .swiper-button-next',
+            prevEl: '.family-gallery-swiper .swiper-button-prev',
+        },
+    });
+
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    const imagesToEnlarge = document.querySelectorAll('.timeline-slide-content .photo-placeholder img, .gallery-item .photo-placeholder img');
+    const closeLightbox = document.querySelector('.close-lightbox');
+
+    imagesToEnlarge.forEach(image => {
+        image.addEventListener('click', () => {
+            lightbox.style.display = 'flex';
+            lightboxImg.src = image.src;
+        });
+    });
+
+    const close = () => {
+        lightbox.style.display = 'none';
+    }
+
+    closeLightbox.addEventListener('click', close);
+    lightbox.addEventListener('click', (e) => {
+        if (e.target === lightbox) {
+            close();
+        }
+    });
 });
